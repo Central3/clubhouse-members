678ebfff0d314e2814b72f24e3e575bbd9233e1c
Central3 <pawarrsumukha@gmail.com>
2026-08-11 08:52:50 +0530

    feat: add sign-up form view and GET route

---
+++ app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import { fileURLToPath } from "node:url";
+import path from "node:path";
+import expressEjsLayouts from "express-ejs-layouts";
+import signUpRouter from "./routes/signUpRouter.js";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(expressEjsLayouts);
+
+app.get("/", (req, res) => {
+  res.redirect("/sign-up");
+});
+app.use("/sign-up", signUpRouter);
+
+app.listen(PORT, (error) => {
+  if (error) {
+    throw error;
+  }
+  console.log(`Example app listening on port ${PORT}`);
+});
