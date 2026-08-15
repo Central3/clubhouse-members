76d37c0c4c82e99608064f333c1601bf5a2227bc
Central3 <pawarrsumukha@gmail.com>
2026-08-15 12:12:15 +0530

    feat: add vaidation and sanitization for log-in form

--- routes/loginRouter.js
+++ routes/loginRouter.js
@@ -1,9 +1,25 @@
 import express from "express";
+import { validationResult, matchedData } from "express-validator";
+
+import { validateLogIn } from "../validators/authValidator.js";
 
 const loginRouter = express.Router();
 
 loginRouter.get("/", (req, res) => {
-  res.render("login");
+  res.render("login", { data: {}, errors: {} });
+});
+
+loginRouter.post("/", validateLogIn, (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const { password, ...formData } = req.body;
+    return res
+      .status(400)
+      .render("login", { data: formData, errors: errors.mapped() });
+  }
+
+  const data = matchedData(req);
+  console.log(data);
 });
 
 export default loginRouter;
