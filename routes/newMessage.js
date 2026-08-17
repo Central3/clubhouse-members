67e77eb33d30eb938bc3c07d7ae59810e17ca5ee
Central3 <pawarrsumukha@gmail.com>
2026-08-17 22:00:39 +0530

    feat: add validation and sanitization for POST /new-message

--- routes/newMessage.js
+++ routes/newMessage.js
@@ -1,7 +1,34 @@
 import express from "express";
+import { matchedData } from "express-validator";
+
+import { body } from "express-validator";
+import checkValidation from "../middleware/checkValidationMiddleware.js";
+
 const newMessageRouter = express.Router();
+
+const validateMessage = [
+  body("message_title")
+    .trim()
+    .notEmpty()
+    .withMessage("Message title is required"),
+  body("message_content").notEmpty().withMessage("Message content is required"),
+];
+
 newMessageRouter.get("/", (req, res) => {
   res.render("new-message");
 });
+
+newMessageRouter.post(
+  "/",
+  validateMessage,
+  checkValidation("new-message"),
+  (req, res) => {
+    const data = matchedData(req);
+    console.log(
+      `New message from ${req.user.username}: ${data.message_content}`
+    );
+    res.redirect("/");
+  }
+);
 
 export default newMessageRouter;
